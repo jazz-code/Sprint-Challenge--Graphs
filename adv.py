04819b4bfb2a4983ef6352183b9759d327363076
ren.meta@gmail.com <ren.meta@gmail.com>
2020-01-26 07:46:48 -0500

    Tests passing, traversal working - needs optimization

--- adv.py
+++ adv.py
@@ -36,11 +36,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -129,36 +129,36 @@ visited_rooms = set()
 # Put starting room on the stack
 room_id = player.current_room.id
 
-def bfs(starting_room, destination_room):
-        """
-        Return a list of direction to follow to get from
-        starting room to destination room
-        """
-        queue = Queue()
-        paths = []
-        visited = set()
-        queue.enqueue([starting_room])
+# deffs(starting_room, destination_room):
+#         """
+#         Return a list of direction to follow to get from
+#         starting room to destination room
+#         """
+#         queue = Queue()
+#         paths = []
+#         visited = set()
+#         queue.enqueue([starting_room])
 
-        while queue.size() > 0:
-            path = queue.dequeue()
-            room = path[-1]
-            if room not in visited:
-                if room == destination_room:
-                    return
-                visited.add(room)
+#         while queue.size() > 0:
+#             path = queue.dequeue()
+#             room = path[-1]
+#             if room not in visited:
+#                 if room == destination_room:
+#                     return
+#                 visited.add(room)
 
-                for next_room in list(traversal_graph[room].values()):
-                    if next_room != '?':
-                        new_path = list(path)
-                        new_path.append(next_room)
-                        queue.enqueue(new_path)
+#                 for next_room in list(traversal_graph[room].values()):
+#                     if next_room != '?':
+#                         new_path = list(path)
+#                         new_path.append(next_room)
+#                         queue.enqueue(new_path)
 
-        for i in range(len(path) - 1):
-            for path, room in traversal_graph[path[i]]:
-                if room == path[i+1]:
-                    paths.append(path)
+#         for i in range(len(path) - 1):
+#             for path, room in traversal_graph[path[i]]:
+#                 if room == path[i+1]:
+#                     paths.append(path)
 
-        return print(f"Paths: ",{paths})
+#         return print(f"Paths: ",{paths})
 
 # print(traversal_path)
 # #       For each edge in the item
@@ -184,6 +184,96 @@ def bfs(starting_room, destination_room):
 
 # traversal_path = []
 
+# # TRAVERSAL TEST
+# visited_rooms = set()
+# player.current_room = world.starting_room
+# visited_rooms.add(player.current_room)
+
+# for move in traversal_path:
+#     player.travel(move)
+#     visited_rooms.add(player.current_room)
+
+# if len(visited_rooms) == len(room_graph):
+#     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+# else:
+#     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+#     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+
+#### Plan ####
+
+# Find Random unexplored direction from the player's current room
+
+# Travels and logs(append) that direction
+
+# Hit dead-end (already explored path)
+
+# Keep track of traversal path (copy), seperate array
+# Loops back to nearest room with unexplored path
+
+# BFS shortest path
+
+
+
+# traversal_path = []
+# stack = Stack()
+# visited_rooms = set()
+# # print(player.current_room.id)
+# # player.current_room = starting_room
+# # Put starting room on the stack
+# stack.push(player.current_room.get_exits())
+# # While stuff in stack
+# while stack.size() > 0:
+# #   Pop the first item
+#     path = stack.pop()
+#     room = path[-1]
+# #    If not visited
+#     if room not in visited_rooms:
+#         visited_rooms.add(room)
+#         traversal_path.append(visited_rooms)
+#         print("test", (traversal_path))
+# print(traversal_path)
+# # #       For each edge in the item
+#     for next_room in sel 
+#     # self.get_neighbors(vertex):
+#     # Copy path to avoid pass by reference bug
+#         traversal_path = list(path) # Make a copy of path rather than reference
+#         traversal_path.append(next_room)
+#         stack.push(traversal_path)
+
+#       For each edge in the item
+        # for next_vert in self.get_neighbors(vertex):
+        # # Copy path to avoid pass by reference bug
+        #     new_path = list(path) # Make a copy of path rather than reference
+        #     new_path.append(next_vert)
+        #     stack.push(new_path)
+
+### Plan ####
+
+# Find Random unexplored direction from the player's current room
+
+# Travels and logs(append) that direction
+
+# Hit dead-end (already explored path)
+
+# Keep track of traversal path (copy), seperate array
+# Loops back to nearest room with unexplored path
+
+# BFS shortest path
+
+traversal_path = []
+
+while len(visited_rooms) < len(room_graph):
+    #  Pick Random unexplored direction from the player's current room
+    direction = random.choice(player.current_room.get_exits())
+    # Move in that direction
+    player.travel(direction)
+    # Log our room in our visited_rooms
+    visited_rooms.add(player.current_room)
+    # Log the direction in our traversal_path
+    traversal_path.append(direction)
+
+
+
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -198,19 +288,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-#### Plan ####
-
-# Find Random unexplored direction from the player's current room
-
-# Travels and logs that direction
-
-# Hit dead-end collision (already explored path)
-
-# Loops back to nearest room with unexplored path
-
-# BFS shortest path
-
 
 #######
 # UNCOMMENT TO WALK AROUND
